2a0097a36f5d8e93c15dd00df5dce31837de5531
Vinicius Marques Regitano <116383168+viniciusregitano@users.noreply.github.com>
2023-02-13 22:36:21 -0300

    njit added and clearer code

--- giant_steps_puzzle_11.py
+++ giant_steps_puzzle_11.py
@@ -4,12 +4,14 @@ Created on Mon Feb 13 14:16:45 2023
 
 @author: Vinicius Marques Regitano
 """
-
+from numba import njit
 import numpy as np
+from datetime import datetime
 
+@njit
 def egg_dropping_fill_matrix(n_floors, n_eggs):
     
-    egg_max_trials_matrix = np.zeros((n_floors +1, n_eggs +1 ))
+    egg_trials_matrix = np.zeros((n_floors +1, n_eggs +1 ))
     
     for floors in range(n_floors +1):
         for eggs in range(n_eggs+1):
@@ -17,39 +19,44 @@ def egg_dropping_fill_matrix(n_floors, n_eggs):
         
             
             # Basic cases: 
-            #     - Cases we have 0 floor left we don't need any more trials - already 0
+            #     - Cases we have 0 egg we'll need no (0) trial
+            #     - Cases we have 1 egg left we'll need the number of floors of trials
             #     - Cases we have 1 floor left we'll need 1 more trial
-            #     - Last basic case is when we have 1 egg and x floors left - we'll need x attemps
+                        
+            if eggs == 0:
+                egg_trials_matrix[0][eggs] = 0
+                continue
             
-            if eggs == 1:
-                egg_max_trials_matrix[floors][1] = floors
-            
-            elif eggs == 0:
-                egg_max_trials_matrix[0][eggs] = 0
+            elif eggs == 1:
+                egg_trials_matrix[floors][1] = floors
+                continue
             
             elif floors == 1:
-                egg_max_trials_matrix[1][eggs] = 1
+                egg_trials_matrix[1][eggs] = 1
+                continue
                 
             # If no basic cases, we'll fill the matrix calculating the elements using the previous calculated elements
-            # In order to already have the calculated elements, we need to calculate from top-left to bottom-right
+            # In order to already have the elements calculated, we need to calculate from top-left to bottom-right
             
             else:
                 
                 # An out of range possible number of trials
-                egg_max_trials_matrix[floors][eggs] = n_floors + 1
+                egg_trials_matrix[floors][eggs] = n_floors + 1
                     
                 for possible_floor in range(floors):
                     
-                    case_egg_breaks = egg_max_trials_matrix[possible_floor-1][eggs-1]
+                    case_egg_breaks = egg_trials_matrix[possible_floor-1][eggs-1] + 1
                     
-                    case_egg_non_break= egg_max_trials_matrix[floors-possible_floor][eggs]
+                    case_egg_non_break= egg_trials_matrix[floors-possible_floor][eggs] + 1
                     
-                    possible_number_trials = 1 + max(case_egg_breaks, case_egg_non_break)
+                    possible_number_trials = max(case_egg_breaks, case_egg_non_break)
                     
-                    egg_max_trials_matrix[floors][eggs] = min(egg_max_trials_matrix[floors][eggs], possible_number_trials)
+                    egg_trials_matrix[floors][eggs] = min(egg_trials_matrix[floors][eggs], possible_number_trials)
+                    
                 
-    return egg_max_trials_matrix
-                
+    return egg_trials_matrix
+
+
 
 if __name__=="__main__":
     
